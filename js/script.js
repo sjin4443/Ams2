7e7e303e366f456293b9405615d508b9328fa097
sjin4443 <71583444+sjin4443@users.noreply.github.com>
2020-10-24 22:57:42 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+    $(".fullsize-img").click(function(){
+        $(".bike").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".fullsize-img").click(function(){
+        $(".cloud1").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".fullsize-img").click(function(){
+        $(".cloud2").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".fullsize-img").click(function(){
+        $(".cloud3").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".fullsize-img").click(function(){
+        $(".tree1").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".fullsize-img").click(function(){
+        $(".tree2").css({
+        'display':'inline-block'
+            })
+    })    
+    
+    $(".fullsize-img").click(function(){
+        $(".boat").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".cloud1").click(function(){
+        $(".cloud1_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".cloud2").click(function(){
+        $(".cloud2_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".cloud3").click(function(){
+        $(".cloud3_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".tree1").click(function(){
+        $(".tree1_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".tree2").click(function(){
+        $(".tree2_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".bike").click(function(){
+        $(".bike_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".boat").click(function(){
+        $(".boat_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+
+})
